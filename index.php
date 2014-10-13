c839ad1ef955a9fedee7e9c0fbf59d2acb597199
Jon Phillips <jon@fabricatorz.com>
2014-10-13 15:27:33 +0200

    updated

--- index.php
+++ index.php
@@ -25,26 +25,41 @@ if ( ! $requirements_met )
 
 // guess at what is the from field
 $from = '';
-if ( !empty($requests['email']) )
-   $from = $requests['email'];
-elseif ( !empty($requests['email-address']) )
-   $from = $requests['email-address'];
-elseif ( !empty($requests['from']) )
-   $from = $requests['from'];
+if ( !empty($inputs['email']) )
+   $from = $inputs['email'];
+elseif ( !empty($inputs['email-address']) )
+   $from = $inputs['email-address'];
+elseif ( !empty($inputs['from']) )
+   $from = $inputs['from'];
+
+
+
+/*
+echo "<pre>";
+var_dump($inputs);
+var_dump($configs);
+echo "</pre>";
+*/
 
 $body_display = '';
-$body = $configs['_success'] . "\n\n";
-$body_party_b = "A form has been submitted.\n\n";
 echo '<table class="table table-striped table-bordered table-hover">' . "\n";
 foreach ( $inputs as $key => $value )
 {
-    $body_display .= '<tr><td>' . strtr(ucfirst($key), '-', ' ') . '</td>' . "<td>$value</td></tr>\n";
-    $body         .= strtr(ucfirst($key), '-', ' ') . ": $value\n";
-    $body_party_b .= strtr(ucfirst($key), '-', ' ') . ": $value\n";
+    $body_display .= '<tr><td style="font-weight: bold">' . strtr(ucfirst($key), '-', ' ') . '</td>' . "<td>$value</td></tr>\n";
 }
 echo '<h3 class="alert alert-success">' . $configs['_success'] . "</h3>\n";
 echo $body_display;
 echo "</table>\n";
+
+
+// email construction
+$body = $configs['_success'] . "\n\n";
+$body_party_b = "A form has been submitted.\n\n";
+foreach ( $inputs as $key => $value )
+{
+    $body         .= strtr(ucfirst($key), '-', ' ') . ": $value\n";
+    $body_party_b .= strtr(ucfirst($key), '-', ' ') . ": $value\n";
+}
 
 // remail($to, $from, $subject, $message, $files);
 // email PARTY A
